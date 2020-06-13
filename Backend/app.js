126d7bb71c35332abfdef0d0bc5e1954d4cd45b2
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2020-06-12 20:12:49 -0500

    babel

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
 const app = express();
 
 // Middelware
@@ -10,14 +11,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', function (req, res) {
+  res.send('Hello World! node');
+});
+
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.send('Hello World! node');
-});
 
 
 app.set('puerto', process.env.PORT || 3000);
