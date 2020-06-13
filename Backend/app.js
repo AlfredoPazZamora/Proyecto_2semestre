5660030960d446220a78b6b9f501f48e942301da
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2020-06-12 19:59:43 -0500

    instalacion de CORS, MORGAN y JSON

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+
+// Middelware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
   res.send('Hello World! node');
 });
+
 
 app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), function () {
