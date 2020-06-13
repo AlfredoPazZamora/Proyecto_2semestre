efaf1089bbe38b0b0d7b40c70a4a2e36244b02b5
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2020-06-12 20:04:17 -0500

    instalacion de vue history

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
